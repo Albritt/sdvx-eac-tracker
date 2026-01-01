3a8f2b5fa3b8c7fe7d21eae08f3a805b872e0b5f
Albritt <devin.albritton@gmail.com>
2026-01-01 13:03:19 -0500

    Add offline tag extracting file for testing

---
+++ offline_url_extract.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import os
+import re
+
+
+def main():
+    html_doc = []
+    with open('html_doc.txt') as file:
+        html_doc = file.read()
+
+    soup = BeautifulSoup(html_doc,'html.parser')
+    for tag in soup.find_all(class_="music"):
+        genre = tag.find(class_=re.compile("genre *")).text
+        info = tag.find("div", class_ = "info")
+        sub_info = info.p
+        title = sub_info.text
+        artist = info.find_next("p").find_next("p").text
+        levels_dict = {}
+        levels = tag.find("div", class_="level")
+        for p in levels.find_all("p"):
+            diff = p["class"][0].upper()
+            level = int(p.get_text(strip=True))
+            levels_dict[diff] = level
+        ptags = tag.find_all("p")
+        pack = ptags[-1].get_text()
+        print(f"Title: {title} ; Artist: {artist} ; Genre: {genre} ; Pack:{pack} ; Levels:{levels_dict}")
+
+if __name__ == "__main__":
+    main()
