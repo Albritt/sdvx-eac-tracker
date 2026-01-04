ba6744c8d3ac286272388ca7ab912eaaf6fa3d6e
Albritt <devin.albritton@gmail.com>
2026-01-04 16:16:13 -0500

    Finish extracting data from website

--- url_extractor.py
+++ url_extractor.py
@@ -1,26 +1,56 @@
 import requests
 from bs4 import BeautifulSoup, Tag
 from requests_ratelimiter import LimiterSession
+from typing import Any
 import re
 import os
+import json
 
-def get_songs_data(text: str) -> dict:
-    soup = BeautifulSoup(text)
+base_url = 'https://p.eagate.573.jp/'
+
+def get_songs_data(text: str) -> list[dict]:
+    metadata = []
+    soup = BeautifulSoup(text, 'html.parser')
     if soup.find(class_="music"):
        for tag in soup.find_all(class_="music"):
-           get_song_metadata(tag)
-           
-       return {}
-    return {}
+           metadata.append(get_song_metadata(tag))
+       return metadata
+    return []
 
-def get_song_metadata(tag: Tag):
+def get_song_metadata(tag: Tag) -> dict[str, Any]:
     genre = tag.find(class_=re.compile("genre *")).text
     info = tag.find("div", class_ = "info")
     title = info.find_next("p").text
     artist = info.find_next("p").find_next("p").text
     levels = get_levels(tag)
     pack = tag.find_all("p")[-1].get_text()
-    music_id = get_music_id()
+    music_id_url = get_music_id_url()
+    #music_id = get_music_id()
+
+    id_pattern = 'music_id='
+    idx = str(music_id_url).index(id_pattern)
+    if idx:
+        idx = len(id_pattern) + idx 
+        music_id = str(music_id_url[idx:])
+    session = LimiterSession(per_second=1)
+    headers = { 'host': 'p.eagate.573.jp',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36'}
+    response = session.get(url=base_url + music_id_url, headers=headers)
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response, 'html.parser')
+    sub_tags = []
+    for sub_tag in soup.find_all(class_="jk"):
+        sub_tags.append(sub_tag.find("img").get("src"))
+    for idx, value in enumerate(levels.values()):
+        value.append(sub_tags[idx])
+    return {
+            'title': title,
+            'artist': artist,
+            'genre': genre,
+            'pack': pack,
+            'music_id': music_id,
+            'levels': levels     
+            }
 
 def get_levels(tag: Tag) -> dict[str, list]:
     levels: dict[str,list] = {}
@@ -31,22 +61,17 @@ def get_levels(tag: Tag) -> dict[str, list]:
         levels[diff] = [level]
     return levels
 
-def get_music_id(tag: Tag):
+def get_music_id_url(tag: Tag):
     jk_div = tag.find("div", class_ = "jk")
-    music_id_url = jk_div.find("a").get('href')
-    id_pattern = 'music_id='
-    idx = music_id_url.index(id_pattern)
-    if idx:
-        idx = len(id_pattern) + idx 
-        return str(music_id_url[idx:])
-    return ""
+    return jk_div.find("a").get('href')
+
 
 
 
 
 
 def main():
-    url = 'https://p.eagate.573.jp/game/eacsdvx/vi/music/index.html'
+    url = url = 'https://p.eagate.573.jp/game/eacsdvx/vi/music/index.html'
     payload = {'search_category': '', 'search_name': '', 'search_level': '13', 'search_condition': '', 'page': '2'}
     headers = { 'host': 'p.eagate.573.jp',
                 'origin': 'https://p.eagate.573.jp',
@@ -57,14 +82,15 @@ def main():
 
     page = 1
     empty_page = False
-    songs = {}
+    songs = []
     while empty_page is False:
         payload = {'search_category': '', 'search_name': '', 'search_level': '', 'search_condition': '', 'page': page}
         response = session.post(url=url, data=payload)
         response.encoding = 'utf-8'
-        data = get_songs_data()
+        data = get_songs_data(response.text)
         if data:
-            songs.update(data)
+            for song in data:
+                songs.extend(song)
             page+=1
         else:
             empty_page = True
