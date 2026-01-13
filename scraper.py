414e4aaa1d9b7fdf8a0a1b71e6ac16df111066fc
Albritt <devin.albritton@gmail.com>
2026-01-13 12:29:54 -0500

    Fix bugs related to adding jacket urls to charts dict and extending the list of songs from each page

--- scraper.py
+++ scraper.py
@@ -16,9 +16,10 @@ def parse_song_results(text: str, domain_name: str, headers: dict) -> list[dict]
     if soup.find(class_="music"):
        for tag in soup.find_all(class_="music"):
            song_data = get_song_metadata(tag)
-           subpage = get_song_subpage(song_data['music_id_url'], domain_name)
-           jacket_urls = get_song_jacket_urls(subpage, domain_name, headers)
-           add_jackets_to_charts(song_data,jacket_urls)
+           subpage = get_song_subpage(song_data['music_id_url'], domain_name, headers)
+           jacket_urls = get_song_jacket_urls(subpage, domain_name)
+           add_jackets_to_charts(song_data['charts'],jacket_urls)
+           metadata.append(song_data)
        return metadata
     return []
 
@@ -36,7 +37,7 @@ def get_song_metadata(tag: Tag) -> dict[str, Any]:
     artist = info.find_next("p").find_next("p").text
     charts = get_charts(tag)
     pack = tag.find_all("p")[-1].get_text()
-    music_id_url = get_music_id_url()
+    music_id_url = get_music_id_url(tag)
     music_id = get_music_id(music_id_url)
 
     return {
@@ -59,9 +60,9 @@ def get_charts(tag: Tag) -> dict[str, dict[str, int]]:
     charts: dict[str,dict[str, int]] = {}
     charts_tag = tag.find("div", class_="level")
     for p in charts_tag.find_all("p"):
-        diff = p["class"][0].upper()
+        diff_name = p["class"][0].upper()
         chart_level = int(p.get_text(strip=True))
-        charts[diff] = {'level' : chart_level}
+        charts[diff_name] = {'level' : chart_level}
     return charts
 
 def get_music_id(music_id_url: str):
@@ -93,10 +94,9 @@ def scrape_sdvx(url: str, headers:dict, domain_name: str,
          'search_level': str(search_level), 'search_condition': pack_name, 'page': page}
         response = session.post(url=url, data=payload)
         response.encoding = 'utf-8'
-        data = parse_song_results(response.text, domain_name)
+        data = parse_song_results(response.text, domain_name, headers)
         if data:
-            for song in data:
-                songs.extend(song)
+            songs.extend(data)
             page+=1
         else:
             empty_page = True
