b862f131058516fed217f655494c679c868e253d
Albritt <devin.albritton@gmail.com>
2026-01-21 06:45:37 -0500

    Change to use relative directory

--- data.py
+++ data.py
@@ -2,12 +2,12 @@ import json
 from pathlib import Path
 
 
-def write_to_json(songs:list[dict], base_dir: str):
+def write_to_json(songs:list[dict], base_dir: str = ""):
     if base_dir == "":
-        path = "/data/sdvx_data.json"
+        path = "./data/sdvx_data.json"
         output_file = Path(path)
     else:
-        path = base_dir + "/data/sdvx_data.json"
+        path = base_dir + "./data/sdvx_data.json"
         output_file = Path(path)
     output_file.parent.mkdir(parents=True,exist_ok=True)
     with open(output_file, 'w', encoding='utf-8') as file:
