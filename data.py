0df674b879d92ccc35b0a7a7174a941bf2753c60
Albritt <devin.albritton@gmail.com>
2026-02-02 18:42:36 -0500

    Extract logic from update_jackets and make functions make_jacket_paths and write_jacket

--- data.py
+++ data.py
@@ -33,15 +33,20 @@ def update_jackets(songs:list[dict[str, Any]], base_dir:str = ""):
         path = base_dir + "/data/jackets/"
     for song in songs:
         music_folder_path = Path(path + song['music_id'])
-        for diff_name in dict(song['charts']).keys():
-            jacket_path = Path(f"{music_folder_path}/{diff_name}.jpg")
-            if jacket_path.is_file():
-                continue
-            else:
-                jacket_url = song['charts'][diff_name]['jacket_url']
-                jacket_path.parent.mkdir(parents=True, exist_ok=True)
-                content = request_jacket(jacket_url)
-                with open(jacket_path, 'wb') as jacket:
-                    jacket.write(content)
+        make_jacket_paths(music_folder_path, song['charts'])
+
+def make_jacket_paths(song_path: Path, charts:dict[str,dict[str,str|int]]):
+    for diff_name in charts.keys():
+        jacket_path = Path(f"{song_path}/{diff_name}.jpg")
+        if jacket_path.is_file():
+            continue
+        else:
+            write_jacket(jacket_path, charts[diff_name]['jacket_url'])
+
+def write_jacket(jacket_path: Path, jacket_url:str):
+    jacket_path.parent.mkdir(parents=True,exist_ok=True)
+    content = request_jacket(jacket_url)
+    with open(jacket_path, 'wb') as jacket:
+        jacket.write(content)
 
 
