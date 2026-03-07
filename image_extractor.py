43ef076a5e4a2abf593da45e7500c05746797578
Albritt <devin.albritton@gmail.com>
2026-03-07 15:41:20 -0500

    Adjust coordinates to be stricter to grab only the field where the music title should be

--- image_extractor.py
+++ image_extractor.py
@@ -1,11 +1,12 @@
 from PIL import Image
 import pytesseract
 
-roi_coords = (385, 980, 484, 1028)
+title_coords = (385, 995, 895, 1027)
+artist_coords = ()
 
 img = Image.open('sv6c_BryONPDgWT.jpg')
 img = img.transpose(Image.ROTATE_270)
-cropped_img = img.crop(roi_coords)
+cropped_img = img.crop(title_coords)
 extracted_title = pytesseract.image_to_string(cropped_img,lang='jpn')
 #img = pytesseract.image_to_string(img,lang='eng+jpn')
 print(extracted_title)
