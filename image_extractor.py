0207a3506a384da4750edca1b9ff854454d9b416
Albritt <devin.albritton@gmail.com>
2026-03-08 15:00:06 -0400

    Add and use artist name coordinates

--- image_extractor.py
+++ image_extractor.py
@@ -2,11 +2,11 @@ from PIL import Image
 import pytesseract
 
 title_coords = (385, 995, 895, 1027)
-artist_coords = ()
+artist_coords = (385, 1030, 895, 1062)
 
 img = Image.open('sv6c_BryONPDgWT.jpg')
 img = img.transpose(Image.ROTATE_270)
-cropped_img = img.crop(title_coords)
+cropped_img = img.crop(artist_coords)
 extracted_title = pytesseract.image_to_string(cropped_img,lang='jpn')
 #img = pytesseract.image_to_string(img,lang='eng+jpn')
 print(extracted_title)
