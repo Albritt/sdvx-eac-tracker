f3c58ceebf2eb7508e773466915346b5617deb17
Devin A <devin.albritton@gmail.com>
2026-03-15 15:44:32 -0400

    Add some code to core file to drive the app

--- core.py
+++ core.py
@@ -1,10 +1,27 @@
 from config import load_config
 from scraper import scrape_sdvx
-
+from data import write_to_json, read_from_json, update_jackets, normalize_data
+from db import setup_db, populate_tables
+import pandas as pd
+import psycopg
+from sqlalchemy import create_engine
+from pathlib import Path
 
 def main():
     config = load_config()
-    songs = scrape_sdvx(url=config['site_url'], headers=config['session_headers'], domain_name=config['domain_name'])
+    #songs = scrape_sdvx(url=config['site_url'], headers=config['session_headers'], domain_name=config['domain_name'],max_page=1)
+    #write_to_json(songs, config['home_dir'] )
+    songs = read_from_json(config['home_dir'])
+    update_jackets(songs, config['home_dir'])
+    music, charts = normalize_data(songs)
+    
+
+    chart_df = pd.DataFrame(charts)
+    music_df = pd.DataFrame(music) 
+    if Path('sdvxkonasute.db').is_file() is False:
+        setup_db()
+    populate_tables(music, charts)
+    #new_songs = read_from_json(config['home_dir'])
 
 if __name__ == "__main__":
     main()
