94afaa4439110dac28cd0962f197969d4af98ab4
Devin A <devin.albritton@gmail.com>
2026-04-05 16:06:28 -0400

    Iterate through directory when processing images rather than single file

--- extractors.py
+++ extractors.py
@@ -44,15 +44,15 @@ def extract_img(path: Path):
     artist_coords = (385, 1030, 895, 1062)
     score_coords = (420, 1065, 807, 1123) 
     
-    img = Image.open('sv6c_BryONPDgWT.jpg')
-    img = img.transpose(Image.ROTATE_270)
-    cropped_img = img.crop(score_coords)
-    extracted_title = pytesseract.image_to_string(cropped_img,lang='jpn')
-    #img = pytesseract.image_to_string(img,lang='eng+jpn')
-    print(extracted_title)
-    #print(img)
-    
-    cropped_img.save('new.jpg')
+    for file in list(path.glob('**/*.png')):
+        img = Image.open(file)
+        img = img.transpose(Image.ROTATE_270)
+        cropped_img = img.crop(score_coords)
+        extracted_title = pytesseract.image_to_string(cropped_img,lang='jpn')
+        #img = pytesseract.image_to_string(img,lang='eng+jpn')
+        print(extracted_title)
+        #print(img)
+        cropped_img.save('new.jpg')
     
 def extract_json(path:Path):
     raise NotImplementedError()
